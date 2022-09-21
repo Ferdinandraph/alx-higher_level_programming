c7ed5cf7e77bb1b4692613ff2676d6afe311b108
root <you@example.com>
2022-09-21 09:11:21 -0700

    file added

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,11 @@
+def to_uperchar(character):
+    if ord(character) >= 97 and ord(character) <= 122:
+        return chr(ord(character) - 32)
+    else:
+        return ord(character)
+
+
+def to_string(string):
+    new_string = ''
+    for x in string:
+        new_string += "%c" % to_uperchar(x)
