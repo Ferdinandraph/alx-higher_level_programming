858848613f8111efa5a855e3eda23c3f60cd3870
root <you@example.com>
2022-09-23 07:36:52 -0700

    file added

--- 0x02-python-import_modules/5-variable_load.py
+++ 0x02-python-import_modules/5-variable_load.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ = "__main__":
     """program that import a variable and and prints it value"""
-    from variable_load_5.py import a
+    from variable_load_5 import a
 
     print(a)
