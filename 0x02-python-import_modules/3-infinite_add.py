8c1d68de897e7d9d7c7f22e9b708f186e9813100
root <you@example.com>
2022-09-23 06:53:55 -0700

    file added

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+if __name__ = "__main__":
+    """progam that adds argument to infinite"""
+    import sys
+
+    a = sys.argv[1:]
+    su = sum(int(i) for i in a)
+    print(su)
