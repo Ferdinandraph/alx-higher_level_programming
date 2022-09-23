94371bd64c727f4db0ad2b1c2626db3838a631bf
root <you@example.com>
2022-09-23 04:42:05 -0700

    error fixed

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,6 +3,9 @@ if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     """add, sub, mul, div 10 and 5"""
 
+    a = 10
+    b = 5
+
     print("{} + {} = {}".format(a, b, add(a, b)))
     print("{} - {} = {}".format(a, b, sub(a, b)))
     print("{} * {} = {}".format(a, b, mul(a, b)))
