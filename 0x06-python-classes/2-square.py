4b1b065aaa500f529a073859383c1377aece58ca
root <you@example.com>
2022-10-11 10:09:26 -0700

    file added

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""a class that defines a square"""
+
+
+class Square:
+    """represent a square"""
+    def __init__(self, size=0):
+        """init initializes the size attribute to the class
+
+        Args:
+        size (int): size of the square
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
