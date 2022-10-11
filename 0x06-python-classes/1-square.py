b3eec74be181a6bd261e9244350bf81cdddbdf58
root <you@example.com>
2022-10-11 09:30:31 -0700

    file added

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""defines a square class"""
+
+
+class Square:
+    """represents a square"""
+    def __init__(self, size):
+        """initializes the size attribute to the square class.
+
+
+        Args:
+        size (int): the size of the square.
+        """
+        self._Square__size = size
