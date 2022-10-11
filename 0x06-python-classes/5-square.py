2c9db09120bd5acf8d479ea1708464f79e192a15
root <you@example.com>
2022-10-11 14:29:29 -0700

    file added

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,39 @@
+"""a class that defines a square"""
+
+
+class Square:
+    """represent a square"""
+    def __init__(self, size=0):
+        """init initializes the size attribute to the class
+
+        Args:
+        size (int): size of the square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """get the current value of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        """set the current value of the square."""
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """return the current area of the square."""
+        return (self.size * self.size)
+
+    def my_print(self):
+        """Print the square with the # character."""
+        for i in range(self.size):
+            for x in range(1, self.size):
+                print("#", end="")
+            print("#")
+        if self.size == 0:
+            print("")
