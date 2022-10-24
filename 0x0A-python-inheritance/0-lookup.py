6ab4dfba2c0cb14451f5b1b00847a923e6282bf1
root <you@example.com>
2022-10-24 14:01:16 -0700

    all fle added

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that returns the list of available attribute and methods"""
+
+
+def lookup(obj):
+    """the lookup function
+   
+
+    Args:
+        obj: contains all methods and attributes"""
+    return dir(obj)
