e7bd4edc8051d873004c17fe46afb25fb93e3e6b
root <you@example.com>
2022-10-24 14:20:37 -0700

    file added

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""function that returns True if the object is exactly an instance of the specified class"""
+
+
+def is_same_class(obj, a_class):
+    """method used in checking if an obj is in class
+
+    Args:
+        obj: an instance the class
+        a_class: the class"""
+    return type(obj) is a_class
