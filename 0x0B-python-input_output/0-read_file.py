929950bb73fb230638178af035fc36e21299c635
root <you@example.com>
2022-10-25 14:09:33 -0700

    all file added

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""function that reads a text file"""
+
+
+def read_file(filename=""):
+    """the function to be implemented"""
+
+    with open(filename, encoding= "utf-8") as fname:
+        print(fname.read())
