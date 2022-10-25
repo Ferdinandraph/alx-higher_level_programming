0bc7bac9b9ce85403b43356644a2a6a0cc7c1a70
root <you@example.com>
2022-10-24 23:26:01 -0700

    correction

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,4 +10,4 @@ def inherits_from(obj, a_class):
         obj: the object
         a_class: the class"""
 
-    return type(obj) is not a_class
+    return issubclass(type(obj), a_class) and type(obj) is not a_class
