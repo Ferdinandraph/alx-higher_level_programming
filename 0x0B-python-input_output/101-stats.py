6a9452f0457568ae54ff794dcea31a694e4eaca1
root <you@example.com>
2022-10-26 15:30:58 -0700

    file commited

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3ads from standard input and computes metrics.
-
 """
 After every ten lines or the input of a keyboard interruption (CTRL + C),
 prints the following statistics:
     - Total file size up to that point.
     - Count of read status codes up to that point.
 """
-
 
 
 def print_stats(size, status_codes):
@@ -19,6 +17,7 @@ def print_stats(size, status_codes):
     print("File size: {}".format(size))
     for key in sorted(status_codes):
         print("{}: {}".format(key, status_codes[key]))
+
 
 if __name__ == "__main__":
     import sys
