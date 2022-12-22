0edd6de9d35489918f36daedd0e33e0c5f81ff37
root <root@HP-OD.localdomain>
2022-12-22 10:46:15 +0100

    file corrected

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-if (process.argv.length <= 3) {
-  console.log(0);
+// script that searches the second biggest integer in the list of argument
+const args = process.argv.slice(2);
+const argsInt = args.map(s => parseInt(s));
+if (args.length === 0 || args.length === 1) {
+  console.log('0');
 } else {
-  const args = process.argv.map(Number)
-    .slice(2, process.argv.length)
-    .sort((a, b) => a - b);
-  console.log(args[args.length - 2]);
+  console.log(argsInt.sort().reverse()[1]);
 }
